abe1bb158087902a5c85f05be4e495f201e395f2
alien-mars <73235550+alien-mars@users.noreply.github.com>
2022-01-17 17:35:11 +0530

    Add files via upload

--- screens/PostCard.js
+++ screens/PostCard.js
@@ -38,122 +38,61 @@ export default class PostCard extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "black"
-  },
-  droidSafeArea: {
-    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
-  },
-  appTitle: {
-    flex: 0.07,
-    flexDirection: "row"
-  },
-  appIcon: {
-    flex: 0.3,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  iconImage: {
-    width: "100%",
-    height: "100%",
-    resizeMode: "contain"
-  },
-  appTitleTextContainer: {
-    flex: 0.7,
-    justifyContent: "center"
-  },
-  appTitleText: {
-    color: "white",
-    fontSize: RFValue(28),
-    fontFamily: "Bubblegum-Sans"
-  },
-  cardContainer: {
-    flex: 0.93,
-    margin : RFValue(13),
-    backgroundColor : '#2A2A2A',
-    borderRadius : RFValue(20),
-  },
-  authorContainer:{
-
-  },
-  authorImageContainer:{
-
-  },
-  profileImage:{
-
-  },
-  authorNameContainer:{
-
-  },
-  authorNameText:{
-    fontSize : RFValue(18),
-    fontFamily : 'Bubblegum-Sans',
-    color : 'white'
-  },
-  postImage:{
-    resizeMode : 'contain',
-    width : "95%",
-    alignSelf : 'center',
-    height : RFValue(250)
-  },
-  captionContainer:{
-
-  },
-  captionText:{
-    fontFamily : 'Bubblegum-Sans',
-    fontSize : 13,
-    color : 'white',
-    paddingTop : RFValue(10)
-  },
-  actionContainer:{
-    justifyContent : 'center',
-    alignItems : 'center',
-    padding : RFValue(10),
-  },
-  likeButton:{
-    width : RFValue(80),
-    height:RFValue(30),
-    justifyContent:'center',
-    alignItems : 'center',
-    flexDirection : 'row',
-    backgroundColor : '#eb3948',
-    borderRadius : RFValue(25)
-  },
-  likeText:{
-    color : 'white',
-    fontSize : RFValue(15),
-    marginLeft : RFValue(5)
-  }
-});
-/*
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  cardContainer:{
-    
-  },
-  storyImage:{
-    
-  },
-  titleContainer:{
-    paddingLeft : RFValue(20),
-    justifyContent : 'center',
-  },
-  storyTitleText:{
-    fontSize : RFValue(18),
-    fontFamily:"Bubblegum-Sans",
-    color : "white"
-  },
-  storyAuthorText:{
-    
-  },
-  descriptionText:{
-    
-  },
-  actionContainer:{
-    
-  },
+    container: {
+      flex: 1,
+      backgroundColor: "black"
+    },
+    cardContainer: {
+      flex: 1,
+      margin : RFValue(20),
+      backgroundColor : '#2A2A2A',
+      borderRadius : RFValue(20),
+    },
+    authorContainer:{
   
-*/
+    },
+    authorImageContainer:{
+  
+    },
+    profileImage:{
+  
+    },
+    authorNameContainer:{
+      padding : RFValue(15)
+    },
+    authorNameText:{
+      fontSize : RFValue(20),
+      color : 'white'
+    },
+    postImage:{
+      resizeMode : 'contain',
+      width : "95%",
+      alignSelf : 'center',
+      height : RFValue(250)
+    },
+    captionContainer:{
+      padding : RFValue(10)
+    },
+    captionText:{
+      fontSize : 13,
+      color : 'white',
+    },
+    actionContainer:{
+      justifyContent : 'center',
+      alignItems : 'center',
+      padding : RFValue(10),
+    },
+    likeButton:{
+      width : RFValue(120),
+      height:RFValue(50),
+      justifyContent:'center',
+      alignItems : 'center',
+      flexDirection : 'row',
+      backgroundColor : '#eb3948',
+      borderRadius : RFValue(25)
+    },
+    likeText:{
+      color : 'white',
+      fontSize : RFValue(20),
+    }
+  });
